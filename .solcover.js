114e8f3b973d3410863dde75ed902aef476f6573
PhABC <ph.castonguay@gmail.com>
2020-10-14 12:15:44 -0400

    Add coverage tool

---
+++ .solcover.js
@@ -0,0 +1,6 @@
+module.exports = {
+  client: require('ganache-cli'),
+  providerOptions: {
+    mnemonic: "concert load couple harbor equip island argue ramp clarify fence smart topic"
+  }
+}
--- buidler.config.ts
+++ buidler.config.ts
@@ -2,6 +2,7 @@ import { usePlugin, BuidlerConfig } from '@nomiclabs/buidler/config'
 
 usePlugin('@nomiclabs/buidler-truffle5')
 usePlugin('buidler-gas-reporter')
+usePlugin('solidity-coverage')
 
 const config: BuidlerConfig = {
   paths: {
@@ -18,6 +19,9 @@ const config: BuidlerConfig = {
     ganache: {
       url: 'http://127.0.0.1:8545',
       blockGasLimit: 10000000
+    },
+    coverage: {
+      url: 'http://localhost:8555'
     }
   },
   gasReporter: {
